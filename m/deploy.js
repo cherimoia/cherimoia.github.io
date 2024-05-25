bdcb2fed982c1651e5eca6b8730954d5c2b92d1d
Kenneth Leung <ccllnekk@outlook.com>
2024-05-25 02:52:39 -0700

    brand new website using massively

---
+++ m/deploy.js
@@ -0,0 +1,206 @@
+(function(global){
+
+  "use strict";
+
+  let YEAR= ""+ new Date().getFullYear();
+  let path = require('path');
+  let fs = require('fs');
+  let TPL="tpl.html";
+  let MNF="../manifest.json";
+  let CHUNK=10;
+  let KL= "../assets/images/kl/";
+  let g_meta;
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  ////////////////////////////////////////////////////////////////////////////
+  function listFiles(subdir,ext){
+    let dir= path.join(__dirname, subdir);
+    let rc=fs.readdirSync(dir);
+    if(ext){
+      ext="."+ext.toLowerCase();
+      rc=rc.filter(n=> n.endsWith(ext));
+    }
+    return rc;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  ////////////////////////////////////////////////////////////////////////////
+  function readFile(f){
+    return fs.readFileSync(path.join(__dirname,f), "utf-8");
+  }
+
+  let INTRO=`
+<div id="intro">
+  <h1>The journey<br />
+  begins...</h1>
+  <ul class="actions">
+    <li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+  </ul>
+</div>
+  `;
+
+  let POST=`
+<article>
+  <header>
+    <span class="date">{{{POST-DATE}}}</span>
+    <h2>{{{POST-DESC}}}</h2>
+  </header>
+  <a href="#" class="image fit"><img src="/assets/images/kl/{{{POST-IMG}}}" alt="" /></a>
+  <div>
+  <span class="fa fa-star checked"></span>
+  <span class="fa fa-star checked"></span>
+  <span class="fa fa-star checked"></span>
+  <span class="fa fa-star"></span>
+  <span class="fa fa-star"></span>
+  </div>
+</article>
+    `;
+
+  let PAGES=`
+<a href="{{{PURL}}}" class="next">Prev</a>
+<a href="{{{NURL}}}" class="next">Next</a>
+  `;
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  ////////////////////////////////////////////////////////////////////////////
+  function delFile(f){
+    return delOneFile(path.join(__dirname, f))
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  ////////////////////////////////////////////////////////////////////////////
+  function delOneFile(f){
+    try{
+      console.log(`deleting file ${f}`);
+      fs.unlinkSync(f)
+    }
+    catch(e){
+    }
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  ////////////////////////////////////////////////////////////////////////////
+  function modGI(info,file){
+    let s=POST;
+    s= s.replace("{{{POST-DATE}}}", fmtDate(info.when));
+    s= s.replace("{{{POST-DESC}}}", info.title);
+    s= s.replace("{{{POST-IMG}}}", file);
+    return s;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  ////////////////////////////////////////////////////////////////////////////
+  function modPage(index, pages, gi,out){
+    let ps,n,p;
+    if(index==1){
+      n="/m/gallery"+(index+1)+".html";
+      p="/m/gallery"+pages+".html";
+      ps=PAGES.replace("{{{PURL}}}",p).replace("{{{NURL}}}",n);
+    }
+    else if(index<pages){
+      n="/m/gallery"+(index+1)+".html";
+      p="/m/gallery"+(index-1)+".html";
+      ps=PAGES.replace("{{{PURL}}}",p).replace("{{{NURL}}}",n);
+    }else{
+      n="/m/gallery"+1+".html";
+      p="/m/gallery"+(index-1)+".html";
+      ps=PAGES.replace("{{{PURL}}}",p).replace("{{{NURL}}}",n);
+    }
+    out=out.replace("{{{PAGES}}}",ps);
+    out=out.replace("{{{POSTS}}}",gi);
+    out=out.replace("{{{YEAR}}}", YEAR);
+    return out;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  ////////////////////////////////////////////////////////////////////////////
+  function doMain(index,pages, root,nums,info){
+    let gi="",
+        px=root,
+        out=root;
+
+    for(let s,m,obj,i=0; i<CHUNK;++i){
+      if(nums.length>0){
+        obj=nums.pop();
+        m=g_meta[obj.f];
+        gi += modGI(m,obj.f) + "\n";
+      }
+    }
+
+    out=out.replace("{{{FADEIN}}}","");
+    out=out.replace("{{{INTRO}}}","");
+    if(index==1){
+      px=px.replace("{{{FADEIN}}}",'class="fade-in"');
+      px=px.replace("{{{INTRO}}}",INTRO);
+    }
+
+    out=modPage(index,pages,gi,out);
+    px=modPage(index,pages,gi,px);
+
+    //write file
+    let p=path.join(__dirname,"gallery"+ String(index)+".html");
+    delOneFile(p);
+    fs.writeFileSync(p,out,"utf-8");
+    //
+    if(index==1){
+      delFile("index.html");
+      fs.writeFileSync( path.join(__dirname,"index.html"),px,"utf-8");
+    }
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  ////////////////////////////////////////////////////////////////////////////
+  const Months={1:"Jan",2:"Feb",3:"Mar",4:"Apr",5:"May",6:"Jun",7:"Jul",8:"Aug",9:"Sep",10:"Oct",11:"Nov",12:"Dec"};
+  function fmtDate(s){
+    let e= `Bad Date ${s}`;
+    if(s.length != 8) {throw e}
+    let year=parseInt(s.substring(0,4));
+    let mon=parseInt(s.substring(4,6));
+    let day=parseInt(s.substring(6,8));
+    if(!(day>0&&day<32) && (mon>0&&mon<13) && (year>1965)) {throw e}
+    return `${Months[mon]} ${day}, ${year}`;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+  //--------------- code starts --------
+  ////////////////////////////////////////////////////////////////////////////
+  let nums= listFiles(KL,"jpg").map(x=>{
+    let p=x.substring(0,x.lastIndexOf("."));
+    let z=/[0-9]+/.test(p);
+    return z? {n:parseInt(p),f:x}: null;
+  });
+
+  if(nums.some(x=> x===null)){
+    return console.log("some file names are not numbered.");
+  }
+
+  nums.sort(function(a,b){return a.n<b.n?-11:a.n>b.n?1:0});
+  //nums.forEach(c=>console.log(c));
+  console.log("number of files= " + nums.length);
+
+  let pages= Math.floor(nums.length/CHUNK);
+  if((nums.length%CHUNK)>0) pages+=1;
+  console.log("pages="+pages);
+
+  let root= readFile(TPL).replaceAll("\r\n","\n");
+  //console.log(root);
+
+  g_meta= JSON.parse(readFile(MNF));
+  //console.log(JSON.stringify(meta));
+
+  for(let i=1;i<=pages;++i){
+    doMain(i,pages,root,nums, {id:"kl",artist:"Josen"});
+  }
+
+})(this);
+
+
+
---
+++ s/deploy.js
@@ -0,0 +1,132 @@
+(function(global){
+
+  "use strict";
+
+  let path = require('path');
+  let fs = require('fs');
+  let TPL="tpl.html";
+  let MNF="../manifest.json";
+  let CHUNK=12;
+  let PAD=0;
+  let KL= "../assets/images/kl/";
+  let g_meta;
+
+  function listFiles(subdir,ext){
+    let dir= path.join(__dirname, subdir);
+    let rc=fs.readdirSync(dir);
+    if(ext){
+      ext="."+ext.toLowerCase();
+      rc=rc.filter(n=> n.endsWith(ext));
+    }
+    return rc;
+  }
+
+  function readFile(f){
+    return fs.readFileSync(path.join(__dirname,f), "utf-8");
+  }
+
+  let GL='<li><a href="/s/gallery${n}.html" title="">Gallery ${n}</a></li>';
+  let GI=`
+  <div class="grid-item">
+      <img class="img-responsive" alt="" src="/assets/images/$[id]/$[item]">
+      <a href="/assets/images/$[id]/$[item]" class="project-description"
+        title="$[title]" class="link-photo project-description"data-lightbox="img">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>$[title]</h3>
+            <p>$[artist]</p>
+          </div>
+        </div>
+      </a>
+    </div>
+    `;
+
+  function delFile(f){
+    return delOneFile(path.join(__dirname, f))
+  }
+  function delOneFile(f){
+    try{
+      fs.unlinkSync(f)
+    }
+    catch(e){
+    }
+  }
+
+  function modGI(id,artist,title,file){
+    let s=GI;
+    s= s.replaceAll("$[id]", id);
+    s= s.replaceAll("$[title]", title);
+    s= s.replaceAll("$[item]", file);
+    return s.replaceAll("$[artist]", artist);
+  }
+
+  function fmtGLinks(pages, out){
+    let gs="";
+    for(let s,i=1;i<pages;++i){
+      s=String(i).padStart(PAD,"0");
+      gs += GL.replaceAll("${n}", ""+s) + "\n";
+    }
+    return out.replace("<!--GALLERY-->",gs);
+  }
+
+  function doMain(index,pages, root,nums,info){
+    let gs="",
+        gi="",
+        out=fmtGLinks(pages, root);
+
+    for(let s,m,obj,i=0; i<CHUNK;++i){
+      if(nums.length>0){
+        obj=nums.pop();
+        m=g_meta[obj.f];
+        gi += modGI(info.id,info.artist,m.title,obj.f) + "\n";
+      }
+    }
+    out= out.replace("<!--GALLERYITEMS-->",gi);
+    //write file
+    let p=path.join(__dirname,"gallery"+ String(index).padStart(PAD,"0")+".html");
+    delOneFile(p);
+    fs.writeFileSync(p,out,"utf-8");
+    //
+    if(index==0){
+      delFile("index.html");
+      fs.renameSync(p, path.join(__dirname,"index.html"));
+    }
+  }
+
+  let nums= listFiles(KL,"jpg").map(x=>{
+    let p=x.substring(0,x.lastIndexOf("."));
+    let z=/[0-9]+/.test(p);
+    return z? {n:parseInt(p),f:x}: null;
+  });
+
+  if(nums.some(x=> x===null)){
+    return console.log("some file names are not numbered.");
+  }
+
+  nums.sort(function(a,b){return a.n<b.n?-11:a.n>b.n?1:0});
+  //nums.forEach(c=>console.log(c));
+  console.log("number of files= " + nums.length);
+
+  let pages= Math.floor(nums.length/CHUNK);
+  if((nums.length%CHUNK)>0) pages+=1;
+  console.log("pages="+pages);
+
+  if(pages<10){ PAD=1 }
+  else if(pages <100) {PAD=2}
+  else if(pages<1000) {PAD=3}
+
+
+  let root= readFile(TPL);
+  //console.log(root);
+
+  g_meta= JSON.parse(readFile(MNF));
+  //console.log(JSON.stringify(meta));
+
+  for(let i=0;i<pages;++i){
+    doMain(i,pages,root,nums, {id:"kl",artist:"Josen"});
+  }
+
+})(this);
+
+
+
